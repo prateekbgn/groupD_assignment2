10a0a59993e0d132277845d904b68b802aacc79b
Mohammed Ameen Abdu Rasheed <92349554+ameyster1999@users.noreply.github.com>
2024-02-01 20:04:20 -0500

    added function to add 2 number by ameen (#1)

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
-package groupD_assignment2
+package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	result := add(3, 7)
+	fmt.Println("Sum:", result)
+
+}
+func add(a, b int) int {
+	return a + b
 }
