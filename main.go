e47409c2f56870251211320c4ca324c455ceda7c
prateekbgn <157074289+prateekbgn@users.noreply.github.com>
2024-02-03 15:34:56 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,14 @@ func main() {
 	fmt.Println("Hello, World!")
 	result := add(3, 7)
 	fmt.Println("Sum:", result)
+	var pnum int
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&pnum)
+	if isPrime(pnum) {
+		fmt.Printf("%d is a prime number.\n", pnum)
+		} else {
+		fmt.Printf("%d is not a prime number.\n", pnum)
+		}
 
 }
 func add(a, b int) int {
@@ -19,4 +27,17 @@ func revString(str string) string {
 		strOutput += theArray[i]
 	}
 	return strOutput
+}
+
+func isPrime(num int) bool {
+	if num < 2 {
+	return false
+	}
+	limit := int(math.Sqrt(float64(num)))
+	for i := 2; i <= limit; i++ {
+		if num%i == 0 {
+		return false
+		}
+	}
+	return true
 }
